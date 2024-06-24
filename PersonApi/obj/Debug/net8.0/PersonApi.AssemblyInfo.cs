0818e39da6f308b919003b933f9b6f68706c1ae2
tomek_1421 <t.wojciechowski.648@studms.ug.edu.pl>
2024-06-24 23:23:38 +0200

    fix api credentials

--- PersonApi/obj/Debug/net8.0/PersonApi.AssemblyInfo.cs
+++ PersonApi/obj/Debug/net8.0/PersonApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c087f3d7fd29d7e6f2c73ff7408fb00ceb5d46d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01edb52771b12dcf2a120ffc76594ba8358b505")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
